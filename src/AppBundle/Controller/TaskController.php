1eb6f2d657436a4b3c7b647ef2f7b6defc60999b
JTorrus <jtorrusgalindo@gmail.com>
2017-11-25 23:16:31 +0100

    function for adding tasks created and view correctly generated with a simple form

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -4,43 +4,56 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Task;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TaskController extends Controller
 {
-    /**
-     * @Route("/Add")
-     */
-    public function AddAction()
+    public function addAction(Request $request)
     {
+        $task = new Task();
+        $task->setTitle('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+          ->add('title', TextType::class)
+          ->add('duedate', DateType::class)
+          ->add('save', SubmitType::class, array('label' => 'Create Task'))
+          ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $task = $form->getData();
+
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($task);
+          $em->flush();
+        }
+
         return $this->render('AppBundle:Task:add.html.twig', array(
-            // ...
+          'form' => $form->createView(),
         ));
     }
 
-    /**
-     * @Route("/Edit")
-     */
-    public function EditAction()
+    public function editAction()
     {
         return $this->render('AppBundle:Task:edit.html.twig', array(
             // ...
         ));
     }
 
-    /**
-     * @Route("/Remove")
-     */
-    public function RemoveAction()
+    public function removeAction()
     {
         return $this->render('AppBundle:Task:remove.html.twig', array(
             // ...
         ));
     }
 
-    /**
-     * @Route("/List")
-     */
-    public function ListAction()
+    public function listAction()
     {
         return $this->render('AppBundle:Task:list.html.twig', array(
             // ...
